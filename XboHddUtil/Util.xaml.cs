720162948ef0b45fc59cb808a595704b659e1eb8
Hatland <LHatland@fillmoreriley.com>
2019-10-17 16:33:25 -0500

    Still just a bare nothingness.
    The void stares deeply into your soul.

--- XboHddUtil/Util.xaml.cs
+++ XboHddUtil/Util.xaml.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,7 +21,7 @@ namespace XboHddUtil
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Util : Page
     {
         Dictionary<string, Guid> guids = new Dictionary<string, Guid>
         {
@@ -31,17 +30,16 @@ namespace XboHddUtil
 
 
 
-        public MainPage()
+        public Util()
         {
             this.InitializeComponent();
 
 
         }
 
-        private async void BtnGetInfo_Click(object sender, RoutedEventArgs e)
+        private void BtnGetInfo_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new MessageDialog("");
-            await dialog.ShowAsync();
+            Console.WriteLine("Test");
         }
     }
 }
