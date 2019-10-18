50f7fa552f07e3f0871720e8ef36371941cdb1ac
Wulf2k <wulf2k@gmail.com>
2019-10-17 19:28:15 -0500

    Behold, a dictionary of GUIDs!
    Isn't this magical?

--- XboHddUtil/MainPage.xaml.cs
+++ XboHddUtil/MainPage.xaml.cs
@@ -26,7 +26,14 @@ namespace XboHddUtil
     {
         Dictionary<string, Guid> guids = new Dictionary<string, Guid>
         {
-            { "BasicPartition", new Guid("EBD0A0A2-B9E5-4433-87C0-68B6B72699C7") }
+            { "BasicPartition", new Guid("EBD0A0A2-B9E5-4433-87C0-68B6B72699C7") },
+            { "Temp", new Guid("B3727DA5-A3AC-4B3D-9FD6-2EA54441011B") },
+            { "User500GB", new Guid("A2344BDB-D6DE-4766-9EB5-4109A12228E5") },
+            { "User1TB", new Guid("25E8A1B2-0B2A-4474-93FA-35B847D97EE5") },
+            { "User2TB", new Guid("5B114955-4A1C-45C4-86DC-D95070008139") },
+            { "SystemSupport", new Guid("C90D7A47-CCB9-4CBA-8C66-0459F6B85724") },
+            { "SystemUpdate", new Guid("9A056AD7-32ED-4141-AEB1-AFB9BD5565DC") },
+            { "SystemUpdate2", new Guid("24B2197C-9D01-45F9-A8E1-DBBCFA161EB2") }
         };
 
 
@@ -40,7 +47,7 @@ namespace XboHddUtil
 
         private async void BtnGetInfo_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new MessageDialog("");
+            var dialog = new MessageDialog(guids["BasicPartition"].ToString());
             await dialog.ShowAsync();
         }
     }
